8740c59e0e8af607af24c3c9693e3f320d9c68aa
Babashov Mahammad <babashov.info@gmail.com>
2023-01-25 23:29:17 +0400

    added 2 test cases.

--- src/components/Todo/Todo.test.js
+++ src/components/Todo/Todo.test.js
@@ -7,16 +7,38 @@ describe('testing todo component',()=>{
 
     beforeEach(()=>{
         const dom = render(<Todo/>)
-        const queryById = queryByAttribute(null,'id')
+        const queryById = queryByAttribute.bind(null,'id')
         fullnameInput = queryById(dom.container,'fullname')
         descriptionInput = queryById(dom.container,'description')
         addTodoButton = screen.getByText('Add Todo')
 
     })
 
-    test(()=>{
+    test('is addTodoButton & fullnameInput & descriptionInput  in the Document',()=>{
+        
+        expect(addTodoButton).toBeInTheDocument()
+        expect(fullnameInput).toBeInTheDocument()
+        expect(descriptionInput).toBeInTheDocument()
+    })
+
+    test('fill fullname input & description textarea & click add button',()=>{
+
+        const fullname = 'Mehemmed'
+        const description = 'Proqramci'
+
+        // Write fullname
+        userEvent.type(fullnameInput,fullname)
+
+        // Write description
+        userEvent.type(descriptionInput,description)
+
+        // Click Add Button
         userEvent.click(addTodoButton)
-        // expect
+
+
+        expect(screen.getByText(/fullname/i)).toBeInTheDocument()
+        expect(screen.getByText(/description/i)).toBeInTheDocument()
+
     })
 
 })
